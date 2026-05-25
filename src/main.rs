e918f5f6ed14b4d13114afe228d5e712176dcc86
haszee <hzeeshan@ualberta.ca>
2026-05-25 00:29:28 -0600

    updated interface handle to include device count & get device. created enum for device flags. updated main() to get interface & device.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sentech_rs::api::{SentechApi, SystemHandle};
+use sentech_rs::api::{DeviceAccess, InterfaceHandle, SentechApi, SystemHandle};
 
 
 // ============================================================================
@@ -30,9 +30,50 @@ fn main() {
         }
     };
 
+    let interface_count = match system.get_interface_count() {
+        Ok(count) => count,
+        Err(e) => {
+            eprintln!("Failed to get interface count: {:?}", e);
+            return;
+        }
+    };
+
+    if interface_count == 0 {
+        eprintln!("No interfaces found.");
+        return;
+    }
+
+    let interface: InterfaceHandle = match system.get_interface(0) { // Zero means get the first interface
+        Ok(interface) => interface,
+        Err(e) => {
+            eprintln!("Failed to get interface: {:?}", e);
+            return;
+        }
+    };
+
+    let access = DeviceAccess::Control;
+
+    let available = match interface.device_available(0, access) { // check if the first device is available
+        Ok(available) => available,
+        Err(e) => {
+            eprintln!("Failed to check device availability: {:?}", e);
+            return;
+        }
+    };
+
+    let dev = match interface.create_first_ist_device(access) {
+        Ok(dev) => dev,
+        Err(e) => {
+            eprintln!("Failed to create first device: {:?}", e);
+            return;
+        }
+    };
+
     println!(
         "Successfully initialized API, created system, and updated interfaces! Re-evaluated: {}",
         reval
     );
+    println!("Device available: {}", available);
+    drop(dev);
 
 }
